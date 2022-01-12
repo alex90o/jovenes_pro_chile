d25252125a0d0d74fd328a4d7b64d56d4de4e7f8
Alexis <alex10o@hotmail.com>
2022-01-12 19:08:15 -0300

    practica con for y while

---
+++ for.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>for...</title>
+</head>
+<body>
+    <script>
+       document.write("Comienza a ejecutar in comando for");
+       for (var contador = 0; contador < 12 ; contador++) {
+           document.write (contador + " ");
+       }
+       document.write("Deje de contar");
+      
+    </script>
+    
+</body>
+</html>
